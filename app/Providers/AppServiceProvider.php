afa5bbb0c2332c8fa8efe37dd1fc0b54adb05c2a
Fabrice OGOU <fabrice.ogou@ngser>
2025-10-11 08:29:16 +0000

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,39 +21,39 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       // Partager les menus avec toutes les vues
-        View::composer('*', function ($view) {
-            $menus = Menu::with(['submenus' => function($q) {
-                $q->select('code', 'libelle', 'target', 'menu_id')
-                    ->where('status', 1)
-                    ->orderBy('position');
-            }])
-            ->select('id', 'code', 'libelle', 'target')
-            ->where('status', 1)
-            ->where('menu_id', 0)
-            ->orderBy('position')
-            ->get()
-            ->map(function($menu) {
-                return [
-                    'code' => $menu->code,
-                    'target' => $menu->target,
-                    'libelle' => $menu->libelle,
-                    'submenus' => $menu->submenus
-                ];
-            })
-            ->toArray();
+    //    // Partager les menus avec toutes les vues
+    //     View::composer('*', function ($view) {
+    //         $menus = Menu::with(['submenus' => function($q) {
+    //             $q->select('code', 'libelle', 'target', 'menu_id')
+    //                 ->where('status', 1)
+    //                 ->orderBy('position');
+    //         }])
+    //         ->select('id', 'code', 'libelle', 'target')
+    //         ->where('status', 1)
+    //         ->where('menu_id', 0)
+    //         ->orderBy('position')
+    //         ->get()
+    //         ->map(function($menu) {
+    //             return [
+    //                 'code' => $menu->code,
+    //                 'target' => $menu->target,
+    //                 'libelle' => $menu->libelle,
+    //                 'submenus' => $menu->submenus
+    //             ];
+    //         })
+    //         ->toArray();
 
-            $view->with('menus', $menus);
-        });
-        $query = Contact::select('opening', 'phone', 'email', 'address', 'map')->first();
-        config(
-            [
-                'opening' => $query->opening,
-                'phone' => $query->phone,
-                'email' => $query->email,
-                'address' => $query->address,
-                'map' => $query->map,
-            ]
-        );
+    //         $view->with('menus', $menus);
+    //     });
+        // $query = Contact::select('opening', 'phone', 'email', 'address', 'map')->first();
+        // config(
+        //     [
+        //         'opening' => $query->opening,
+        //         'phone' => $query->phone,
+        //         'email' => $query->email,
+        //         'address' => $query->address,
+        //         'map' => $query->map,
+        //     ]
+        // );
     }
 }
